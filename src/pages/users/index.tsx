5e4c570249262dff168efcda42e8cefad9df07c6
Gabriel Carvalho <gabrielhenlc@gmail.com>
2022-12-08 09:16:39 -0300

    add responsive users log

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
@@ -7,6 +7,11 @@ import { Pagination } from "../../components/Pagination";
 import { RiAddLine, RiPencilLine } from "react-icons/ri"
 
 export default function UserList() {
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true
+    })
+
     return (
         <Box>
             <Header />
@@ -14,7 +19,7 @@ export default function UserList() {
             <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
                 <Sidebar />
 
-                <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+                <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
                     <Flex mb="8" justify="space-between" align="center">
                         <Heading size="lg" fontWeight="normal">Usuários</Heading>
 
@@ -33,17 +38,18 @@ export default function UserList() {
                     <Table colorScheme="whiteAlpha">
                         <Thead>
                             <Tr>
-                                <Th px="6" color="gray.300" w="8">
+                                <Th px={["4", "4", "6"]} color="gray.300" w="8">
                                     <Checkbox colorScheme="pink" />
                                 </Th>
                                 <Th>Usuário</Th>
-                                <Th>Data de cadastro</Th>
+                                {isWideVersion && <Th>Data de cadastro</Th>}
+
                                 <Th w="8"></Th>
                             </Tr>
                         </Thead>
                         <Tbody>
                             <Tr>
-                                <Td px="6">
+                                <Td px={["4", "4", "6"]}>
                                     <Checkbox colorScheme="pink" />
                                 </Td>
                                 <Td>
@@ -52,7 +58,7 @@ export default function UserList() {
                                         <Text fontSize="sm" color="gray.300">gabrielhenlc@gmail.com</Text>
                                     </Box>
                                 </Td>
-                                <Td>04 de Abril de 2022</Td>
+                                {isWideVersion && <Td>04 de Abril de 2022</Td>}
                                 <Td>
                                     <Button
                                         as="a"
@@ -60,9 +66,64 @@ export default function UserList() {
                                         fontSize="sm"
                                         colorScheme="purple"
                                         cursor="pointer"
+                                        iconSpacing={isWideVersion ? '1.5' : '-0.5'}
                                         leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
                                     >
-                                        Editar
+                                        {isWideVersion ? 'Editar' : ''}
+                                    </Button>
+                                </Td>
+                            </Tr>
+                        </Tbody>
+                        <Tbody>
+                            <Tr>
+                                <Td px={["4", "4", "6"]}>
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">Gabriel Carvalho </Text>
+                                        <Text fontSize="sm" color="gray.300">gabrielhenlc@gmail.com</Text>
+                                    </Box>
+                                </Td>
+                                {isWideVersion && <Td>04 de Abril de 2022</Td>}
+                                <Td>
+                                    <Button
+                                        as="a"
+                                        size="sm"
+                                        fontSize="sm"
+                                        colorScheme="purple"
+                                        cursor="pointer"
+                                        iconSpacing={isWideVersion ? '1.5' : '-0.5'}
+                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                                    >
+                                        {isWideVersion ? 'Editar' : ''}
+                                    </Button>
+                                </Td>
+                            </Tr>
+                        </Tbody>
+                        <Tbody>
+                            <Tr>
+                                <Td px={["4", "4", "6"]}>
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">Gabriel Carvalho </Text>
+                                        <Text fontSize="sm" color="gray.300">gabrielhenlc@gmail.com</Text>
+                                    </Box>
+                                </Td>
+                                {isWideVersion && <Td>04 de Abril de 2022</Td>}
+                                <Td>
+                                    <Button
+                                        as="a"
+                                        size="sm"
+                                        fontSize="sm"
+                                        colorScheme="purple"
+                                        cursor="pointer"
+                                        iconSpacing={isWideVersion ? '1.5' : '-0.5'}
+                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                                    >
+                                        {isWideVersion ? 'Editar' : ''}
                                     </Button>
                                 </Td>
                             </Tr>
