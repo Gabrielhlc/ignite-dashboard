da47d9189d6e5a6bae2cc2579dc7757043f68774
Gabriel Carvalho <gabrielhenlc@gmail.com>
2022-12-10 18:42:54 -0300

    add yup data validation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { Flex, Button, Stack } from '@chakra-ui/react'
 import { SubmitHandler, useForm } from 'react-hook-form'
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup'
 
 import { Input } from '../components/Forms/Input'
 
@@ -8,8 +10,17 @@ type SignInFormData = {
   password: string;
 }
 
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória '),
+})
+
 export default function SignIn() {
-  const { register, handleSubmit, formState } = useForm<SignInFormData>()
+  const { register, handleSubmit, formState } = useForm<SignInFormData>({
+    resolver: yupResolver(signInFormSchema)
+  })
+
+  const { errors } = formState
 
   const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
     await new Promise(resolve => setTimeout(resolve, 2000))
@@ -36,8 +47,8 @@ export default function SignIn() {
       >
         <Stack spacing="4">
 
-          <Input type="email" label="E-mail" {...register("email", { required: "E-mail obrigatório" })} />
-          <Input type="password" label="Senha" {...register("password")} />
+          <Input type="email" label="E-mail" error={errors.email} {...register("email")} />
+          <Input type="password" label="Senha" error={errors.password} {...register("password")} />
         </Stack>
 
         <Button
